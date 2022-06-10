0658b574bf6dad0d6c93afc37ce852e69ec6711b
wandy <wandywahidin53@gmail.com>
2022-06-10 14:59:38 +0700

    Tugas 7

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theater1 = await promiseTheaterIXX()
+  const theater2 = await promiseTheaterVGC()
+  const allTheater = [...theater1, ...theater2]
+
+  const result = allTheater.filter((emo) => emo.hasil === emosi)
+  return result.length
+};
 
 module.exports = {
   promiseOutput,
